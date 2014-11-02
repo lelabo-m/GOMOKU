8851ce4c361de28cf768b1ff2dc7500619b9672a
Le Labourier Marc <marc.lelabourier@gmail.com>
2014-11-02 15:55:44 +0100

    add startup scene

---
+++ UnityGomoku/Assets/Scripts/DelayerSwitchScene.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DelayerSwitchScene : MonoBehaviour
+{
+    public float SecondsOfDelay;
+
+    void Update()
+    {
+        SecondsOfDelay -= Time.deltaTime;
+        if (SecondsOfDelay < 0.0f)
+            Application.LoadLevel(1);
+    }
+}
