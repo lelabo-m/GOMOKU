609d75dd9ef9b2a8b7be0c341378258d3a702c62
Le Labourier Marc <marc.lelabourier@gmail.com>
2014-11-02 17:20:32 +0100

    Add Menu scene + Main Menu

---
+++ UnityGomoku/Assets/Scripts/MenuSwitcherScript.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuSwitcherScript : MonoBehaviour
+{
+    public GameObject MainMenu;
+    public GameObject SetupGameMenu;
+	public GameObject HowtoMenu;
+    public GameObject SettingsMenu;
+    public GameObject CreditsMenu;
+
+    public void Start()
+    {
+        NGUITools.SetActive(SetupGameMenu, false);
+        NGUITools.SetActive(SettingsMenu, false);
+        NGUITools.SetActive(CreditsMenu, false);
+		NGUITools.SetActive(HowtoMenu, false);
+    }
+
+    public void ShowSetupGame()
+    {
+        Application.LoadLevel(2);
+    }
+
+	public void ShowHowto()
+	{
+		NGUITools.SetActive(MainMenu, false);
+		NGUITools.SetActive(HowtoMenu, true);
+	}
+
+    public void ShowSettings()
+    {
+        NGUITools.SetActive(MainMenu, false);
+		NGUITools.SetActive(SettingsMenu, true);
+    }
+
+    public void ShowCredits()
+    {
+        NGUITools.SetActive(MainMenu, false);
+		NGUITools.SetActive(CreditsMenu, true);
+    }
+	public void ShowMenuQuitCredits()
+	{
+		NGUITools.SetActive(MainMenu, true);
+		NGUITools.SetActive(CreditsMenu, false);
+	}
+	public void ShowMenuQuitHowto()
+	{
+		NGUITools.SetActive(MainMenu, true);
+		NGUITools.SetActive(HowtoMenu, false);
+	}
+	public void ShowMenuQuitSettings()
+	{
+		NGUITools.SetActive(MainMenu, true);
+		NGUITools.SetActive(SettingsMenu, false);
+	}
+}
