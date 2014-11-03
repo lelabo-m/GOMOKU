ce6b78a6f2c6b6f1bb5073a972e2e7ac63bc36cd
hadrien-lejard <hadrien.lejard@gmail.com>
2014-11-03 11:38:31 +0100

    Prefabs

--- UnityGomoku/Assets/Scripts/MapComponent.cs
+++ UnityGomoku/Assets/Scripts/MapComponent.cs
@@ -1,73 +1,95 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MapComponent : MonoBehaviour {
+public class MapComponent : MonoBehaviour
+{
 		
-	public const int SIZE_MAP = 19;
-	public enum CellType { Empty, White, Black };
-
-	private BitsMap bitsMap;
-	private char[] map;
+		public const int SIZE_MAP = 19;
+		public enum Color
+		{
+				Empty,
+				White,
+				Black }
+		;
+		private GameObject arbiter;
+		private Rules rules;
+		private BitsMap bitsMap;
+		public char[] map;
 	
-	// Use this for initialization
-	void Start () {
-		bitsMap = new BitsMap ();
-		map = new char[SIZE_MAP];
-
-	}
+		// Use this for initialization
+		void Start ()
+		{
+				bitsMap = new BitsMap ();
+				map = new char[SIZE_MAP];
+				arbiter = GameObject.Find ("Arbiter");
+				rules = (Rules)arbiter.GetComponent ("Rules");
+		}
 	
-	// Update is called once per frame
-	void Update () {
+		// Update is called once per frame
+		void Update ()
+		{
 	
-	}
+		}
 	
-	public bool putPawn(int x, int y, CellType type) {
-		bitsMap.putPawn (x, y, type);
-		map [x * SIZE_MAP + y] = (char) type;
-		return true;
-	}
+		public bool putPawn (int x, int y, Color type)
+		{
+				if (!rules.putPawn (bitsMap, x, y))
+						return false;
+				bitsMap.putPawn (x, y, type);
+				map [x * SIZE_MAP + y] = (char)type;
+				return true;
+		}
 
-	public bool removePawn(int x, int y) {
-		bitsMap.removePawn (x, y);
-		map [x * SIZE_MAP + y] = (char) CellType.Empty;
-		return true;
-	}
+		public bool removePawn (int x, int y)
+		{
+				bitsMap.removePawn (x, y);
+				map [x * SIZE_MAP + y] = (char)Color.Empty;
+				return true;
+		}
 
-	public char getCell(int x, int y) {
-		return map [x * SIZE_MAP + y];
-	}
+		public char getCell (int x, int y)
+		{
+				return map [x * SIZE_MAP + y];
+		}
 
-	// return map converted
-	public char[] getMap() {
-		return map;
-	}
+		// return map converted
+		public char[] getMap ()
+		{
+				return map;
+		}
 
-	public BitsMap getBitsMap() {
-		return bitsMap;
-	}
+		public BitsMap getBitsMap ()
+		{
+				return bitsMap;
+		}
 
-	public class BitsMap { 
+		public class BitsMap
+		{ 
 		
-		private int[] map = new int[SIZE_MAP];
+				private int[] map = new int[SIZE_MAP];
 		
 		
-		// set Weight of a color 
-		public void setWeight(int weight, CellType color) {
+				// set Weight of a color 
+				public void setWeight (int weight, Color color)
+				{
 			
-		}
+				}
 		
-		public bool putPawn(int x, int y, CellType type) {
-			return true;
-		}
+				public bool putPawn (int x, int y, Color type)
+				{
+						return true;
+				}
 		
-		public bool removePawn(int x, int y) {
-			return true;
-		}
+				public bool removePawn (int x, int y)
+				{
+						return true;
+				}
 		
-		public int getCell(int x, int y) {
-			return map [x * SIZE_MAP + y];
-		}
+				public int getCell (int x, int y)
+				{
+						return map [x * SIZE_MAP + y];
+				}
 		
-	}
+		}
 
 }
