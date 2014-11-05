aecbe4f27c13d5e30fb348cfba9a3afd5aa654a0
kunt22 <guillaumejparis@gmail.com>
2014-11-05 18:06:49 +0100

    settingsUser

---
+++ UnityGomoku/Assets/Scripts/SettingsUser.cs
@@ -0,0 +1,28 @@
+﻿	using UnityEngine;
+	using System.Collections;
+
+	public class SettingsUser : MonoBehaviour {
+
+		public void OnChangeCinq (){
+		if (PlayerPrefs.GetInt ("5 cassables") == 0 || PlayerPrefs.GetInt ("5 cassables") == 2) {
+				PlayerPrefs.SetInt ("5 cassables", 1);
+			} 
+			else {
+				PlayerPrefs.SetInt ("5 cassables", 0);
+			}
+		}
+
+		public void OnChangeTrois (){
+		if (PlayerPrefs.GetInt ("double 3") == 0 || PlayerPrefs.GetInt ("double 3") == 2) {
+			PlayerPrefs.SetInt ("double 3", 1);
+		} 
+		else {
+			PlayerPrefs.SetInt ("double 3", 0);
+		}
+	}
+	
+	void Start () {
+	
+	}
+
+	}
