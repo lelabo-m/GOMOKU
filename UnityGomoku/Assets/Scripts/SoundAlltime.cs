418b3fb2705f6512011785bd9a0e95bd81cdfaa9
kunt22 <guillaumejparis@gmail.com>
2014-11-05 17:18:10 +0100

    Add sound always

---
+++ UnityGomoku/Assets/Scripts/SoundAlltime.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundAlltime : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void Awake (){
+		DontDestroyOnLoad (this);
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
