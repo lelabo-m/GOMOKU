6193fd00981a406869f86fc755aa45fbdd8e9016
Le Labourier Marc <marc.lelabourier@gmail.com>
2014-11-26 14:45:46 +0100

    DataType Base

---
+++ UnityGomoku/Assets/Scripts/DataType.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+enum Orientation { NORTH, NORTHWEST, WEST, SOUTHWEST, SOUTH, SOUTHEAST, EAST, NORTHEAST }
+
+public class Coord
+{
+    public int x;
+    public int y;
+}
+
+public class Vector
+{
+    public Coord        begin;
+    public Coord        end;
+    public Orientation ori;
+}
+
+public class ThreeFree : Vector
+{
+}
+
+public class Segment : Vector
+{
+}
