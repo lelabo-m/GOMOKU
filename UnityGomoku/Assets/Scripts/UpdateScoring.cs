fa0811f7fbe85aee7f688b300e17ac0d6a1b5ea5
kunt22 <guillaumejparis@gmail.com>
2014-12-14 19:18:05 +0100

    LAst for the day

--- UnityGomoku/Assets/Scripts/UpdateScoring.cs
+++ UnityGomoku/Assets/Scripts/UpdateScoring.cs
@@ -15,10 +15,14 @@ public class UpdateScoring : MonoBehaviour {
 		int val;
 		MapComponent map = GameObject.Find("Map").GetComponent<MapComponent>();
 		Text c = GameObject.Find("score white").GetComponent<Text>();
+		Text cf = GameObject.Find("score whiteE").GetComponent<Text>();
 		val = map.GetMap().scores[(int) Gomoku.Color.White];
 		c.text = val.ToString();
+		cf.text = val.ToString();
 		Text d = GameObject.Find("score black").GetComponent<Text>();
+		Text df = GameObject.Find("score blackE").GetComponent<Text>();
 		val = map.GetMap().scores[(int) Gomoku.Color.Black];
 		d.text = val.ToString();
+		df.text = val.ToString();
 	}
 }
